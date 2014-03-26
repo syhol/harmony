ae193a17ce198c0d3fe22c2ced191fd2364e9070
Simon Holloway <holloway.sy@gmail.com>
2014-03-26 00:28:02 +0000

    another fix for formatting

--- functions/modules/dev-tools/dev-tools.php
+++ functions/modules/dev-tools/dev-tools.php
@@ -4,8 +4,8 @@
  *
  * Module for development debugging and profiling
  * 
- * @package	Dev_Tools
- * @author 	Simon Holloway <holloway.sy@gmail.com>
+ * @package Dev_Tools
+ * @author  Simon Holloway <holloway.sy@gmail.com>
  * @license http://opensource.org/licenses/MIT MIT
  * @version 0.0.1
  */
